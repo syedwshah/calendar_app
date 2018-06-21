3cdd9e85131dd8e1eabb469e5c5d840dcada1976
Syed Shah <syed.shah@nyu.edu>
2018-06-20 20:40:41 -0400

    Basic setup for React

---
+++ client/src/components/Calendar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+// import react-fontawesome
+
+
+export default class Calendar extends Component {
+  render() {
+    return (
+      <div className="calendar">
+        <h2>Calendar</h2>
+      </div>
+    );
+  }
+}
