5b98640c9178000b747d51a6861e61f3e6a01e65
Syed Shah <syed.shah@nyu.edu>
2018-06-24 13:20:16 -0400

    Add events to database

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Calendar from './components/calendar/Calendar';
 import Events from './components/calendar/Events';
+import { base } from './base';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       selectedDateId: null,
-      selectedDate: null
+      selectedDate: null,
+      events: {}
     };
   }
 
@@ -19,6 +21,46 @@ class App extends Component {
     });
   }
 
+  addEvent = (eventId, start, end, details) => {
+    const events = {...this.state.events};
+    if (events[eventId]) {
+      //append to eventId
+      events[eventId].map((currentValue, index) => {
+        if (events[eventId][`${++index}`]) {/*do nothing*/}
+        else {
+          events[eventId][`${index}`] = {
+            id: Date.now(),
+            start_time: start,
+            end_time: end,
+            details: details
+          }
+        }
+      });
+    }
+    else {
+      events[eventId] = {
+        0: {
+          id: Date.now(),
+          start_time: start,
+          end_time: end,
+          details: details
+        }
+      }
+    }
+
+    this.setState({events});
+  }
+
+  componentWillMount() {
+    this.eventsRef = base.syncState('events', {
+      context: this,
+      state: 'events'
+    });
+  }
+  componentWillUnmount() {
+    base.removeBinding(this.eventsRef);
+  }
+
   render() {
     return (
       <div className="App">
@@ -27,7 +69,9 @@ class App extends Component {
           <Calendar
             handleStoreChange={this.handleStoreChange} />
             <hr />
-          <Events selectedDate={this.state.selectedDate}/>
+          <Events selectedDate={this.state.selectedDate}
+            selectedDateId={this.state.selectedDateId}
+            addEvent={this.addEvent} />
         </section>
 
         <section className="all-events">List of all events go here</section>
--- client/src/components/calendar/Events.jsx
+++ client/src/components/calendar/Events.jsx
@@ -4,20 +4,62 @@ import './events.css';
 export default class Events extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      start: '',
+      end: '',
+      details: ''
+    };
+  }
+
+  // handleChange = (e, stateVal) => {
+  //   this.setState({stateVal: e.target.value});
+  // }
+
+  handleChange = (e, stateVal) => {
+    let change = {};
+    change[stateVal] = e.target.value;
+    this.setState(change);
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addEvent(
+      this.props.selectedDateId,
+      this.state.start,
+      this.state.end,
+      this.state.details
+    );
+    this.setState({
+      start: '',
+      end: '',
+      details: ''
+    });
   }
 
   render() {
     return(
       <div className="events">
-        <div>Events {this.props.selectedDate}</div>
+        <div className="close" style={{color: "black", textAlign: "left"}}>
+          <button style={{borderRadius: "50%"}}>X</button>
+        </div>
+        <div>{this.props.selectedDate}</div>
         <br />
-        <div>events on this day</div><button>edit</button><button>delete</button>
-        <div>events on this day</div><button>edit</button><button>delete</button>
-        <div>events on this day</div><button>edit</button><button>delete</button>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Start <input type="text" placeholder="hr:mm am/pm" value={this.state.start} onChange={(e) => (this.handleChange(e, "start"))} />
+          </label>
+          <br />
+          <label>
+            End <input type="text" placeholder="hr:mm am/pm" value={this.state.end} onChange={(e) => (this.handleChange(e, "end"))} />
+          </label>
+          <label>
+            <textarea placeholder="Details for this event" value={this.state.details} onChange={(e) => (this.handleChange(e, "details"))} />
+          </label>
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
 
-        <br />
-        <div>some form goes here</div>
+
       </div>
     );
   }
